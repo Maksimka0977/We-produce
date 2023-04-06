20d9cbdf396dfdf088ca7ff5fd43ab5cf9f6c14b
Maksim <105245976+Maksimka0977@users.noreply.github.com>
2023-04-06 23:59:30 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+const play = document.querySelector('.about__play');
+const video = document.querySelector('.about__video ');
+
+play.addEventListener('click', () => {
+  video.play();
+  video.setAttribute('controls', 'controls');
+  play.classList.add('about__play--hidden');
+});
